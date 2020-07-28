b9f30d0664b09645ecbcb6c2bd7a26ee2b208007
Adeleye Adewale <adeleye@tradersofafrica.com>
2020-07-28 09:39:03 +0100

    first commit

--- bin/www.js
+++ bin/www.js
@@ -5,19 +5,20 @@ const { parse } = require('querystring');
  * Create HTTP server.
  */
 
-http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            res.end(`Hello ${parse(body).name}, Welcome to WeJapa Internships`);
-            // return console.log(body);
-            
-        });
-    }
-    else if(req.method ==='GET'){
-        res.end('Hello World, Welcome to WeJapa Internships');
-    }
-  }).listen(3030);
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(function (req,res){
+    res.statusCode=200
+    res.setHeader('Content-Type','application/json')
+    let body={}
+    body['statusCode'] = res.statusCode;
+    body['Content-Type'] =res.getHeader('Content-Type');
+    body['success'] = true
+    // console.log(body)
+    res.end(JSON.stringify(body));
+})
+
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
